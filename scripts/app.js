f4bd9a9de876d6bf15a7be58be73c713081743c9
Hérson Rezende <hersonrezende2015@gmail.com>
2021-11-03 13:52:49 -0300

    up

--- scripts/app.js
+++ scripts/app.js
@@ -13,29 +13,4 @@ function changeIconsTechnologies(){
     }
 }
 
-function introduzirVideoDosProjetos(){
-    var divBackground = document.getElementById("background");
-    var divProjects = document.getElementById("projects");
-    var video = document.createElement("video");
-    var source = document.createElement("source");
-
-    video.setAttribute("id", "projects-video");
-    video.setAttribute("width", "500");
-    video.setAttribute("height", "240");
-    video.setAttribute("autoplay", "autoplay");
-    video.setAttribute("controls", "controls");
-    video.setAttribute("poster", "media/images/background.png");
-    video.volume = 0.2;
-    video.currentTime = 0;
-
-    source.setAttribute("src", "media/videos/projects.mp4");
-    source.setAttribute("type", "video/mp4");
-
-    video.appendChild(source);
-    divProjects.appendChild(video);
-    divBackground.style.display = "none";
-}
-
 setInterval(changeIconsTechnologies, 2000);
-
-//setTimeout(introduzirVideoDosProjetos, 2000);
