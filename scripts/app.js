4ebf0155540e930cfc133f6916b862384c39f3c1
Hérson Rezende <hersonrezende2015@gmail.com>
2021-11-03 17:04:40 -0300

    Up

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,27 @@
-const icons = ["fab fa-html5", "fab fa-css3", "fab fa-java", "fab fa-js", "fab fa-node-js", "fab fa-react", "fas fa-database", "fab fa-docker", "fab fa-git-alt", "fab fa-trello"];
-var count = 0;
+const icons = [
+    "fab fa-html5", 
+    "fab fa-css3", 
+    "fab fa-java", 
+    "fab fa-js", 
+    "fab fa-node-js", 
+    "fab fa-react", 
+    "fas fa-database", 
+    "fab fa-docker", 
+    "fab fa-git-alt", 
+    "fab fa-trello",
+];
+
+var countIconcs = 0;
 
 function changeIconsTechnologies(){
     //var random = Math.floor(Math.random() * icons.length);
     var icon = document.getElementById("fab-technologies");
 
-    icon.setAttribute("class", icons[count]);
-    count++;
+    icon.setAttribute("class", icons[countIconcs]);
+    countIconcs++;
 
-    if(count >= icons.length){
-        count = 0;
+    if(countIconcs >= icons.length){
+        countIconcs = 0;
     }
 }
-
 setInterval(changeIconsTechnologies, 2000);
